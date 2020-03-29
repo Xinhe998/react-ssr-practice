a65810ab1ed96fba0aa72adbe09a625299a4226e
Xinhe <xinhe998@gmail.com>
2020-03-30 02:50:54 +0800

    init

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+
+// we'll talk about this in a minute:
+import serverRenderer from './middleware/renderer';
+
+const PORT = 3000;
+const path = require('path');
+
+// initialize the application and create the routes
+const app = express();
+const router = express.Router();
+
+// root (/) should always serve our server rendered page
+router.use('^/$', serverRenderer);
+
+// other static resources should just be served as they are
+router.use(express.static(
+    path.resolve(__dirname, '..', 'build'),
+    { maxAge: '30d' },
+));
+
+// tell the app to use the above rules
+// anything else should act as our index page
+// react-router will take care of everything
+router.use('*', serverRenderer);
+app.use(router);
+
+// start the app
+app.listen(PORT, (error) => {
+    if (error) {
+        return console.log('something bad happened', error);
+    }
+
+    console.log("listening on " + PORT + "...");
+});
